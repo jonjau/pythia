2c921b97c56dbdb82f94c574498ed0aad9ebe1f6
Jonathan Jauhari <40555491+jonjau@users.noreply.github.com>
2025-03-13 22:49:58 +1100

    Split partial/full template into own handler functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, error::Error, sync::Arc};
+use std::{collections::HashMap, error::Error};
 
 use askama::Template;
 use askama_axum::IntoResponse;
@@ -16,7 +16,7 @@ mod models;
 mod services;
 mod utils;
 
-use models::{goal::GoalTerm, record_type::RecordType};
+use models::{goal::GoalTerm};
 use services::{
     fact::FactService,
     state_change::{ChangePath, StateChangeService},
@@ -91,70 +91,47 @@ async fn get_state_changes(
     Path(state_rt): Path<String>,
     Query(q): Query<HashMap<String, String>>,
 ) -> StateChangesPage {
-    let get_named_values = |prefix: &str| {
+    let get_values_for_key_with_prefix = |prefix: &str| {
         q.iter()
             .filter_map(|(field, value)| {
-                field.starts_with(prefix).then_some((
-                    field[prefix.len()..].to_string(),
-                    GoalTerm::String(value.to_string()),
-                ))
+                field
+                    .starts_with(prefix)
+                    .then_some((field[prefix.len()..].to_string(), value.to_string()))
             })
             .collect::<HashMap<_, _>>()
     };
 
-    let named_values0 = get_named_values("start.");
-    let named_values1 = get_named_values("end.");
+    let named_values0 = get_values_for_key_with_prefix("start.");
+    let named_values1 = get_values_for_key_with_prefix("end.");
     let n_steps = q.get("num-steps");
 
     if !headers.contains_key("HX-Request") {
-        return StateChangesPage::Input(get_state_changes_input(
-            app_state.facts.get_record_type(&state_rt).await.unwrap(),
-            named_values0,
-            named_values1,
-            n_steps.map(|s| s.parse::<i32>().unwrap_or(0)).unwrap_or(0),
-        ));
+        return StateChangesPage::Input(
+            get_state_changes_input(
+                app_state,
+                state_rt,
+                named_values0,
+                named_values1,
+                n_steps.map(|s| s.parse::<i32>().unwrap_or(0)).unwrap_or(0),
+            )
+            .await,
+        );
     } else {
-        let parse_n_steps = || -> Result<i32, Box<dyn Error>> {
-            let n_steps = n_steps
-                .ok_or("num-steps not found")?
-                .parse::<i32>()
-                .map_err(|_| "Invalid num-steps argument")?;
-            Ok(n_steps)
-        };
-
-        let n_steps = match parse_n_steps() {
-            Ok(i) => i,
-            Err(e) => {
-                return StateChangesPage::Output(StateChangesPageOutput {
-                    error_message: Some(e.to_string()),
-                    paths: vec![],
-                });
-            }
-        };
-
-        match app_state
-            .state_changes
-            .get_paths(&state_rt, named_values0, named_values1, n_steps)
-            .await
-        {
-            Ok(paths) => StateChangesPage::Output(StateChangesPageOutput {
-                error_message: None,
-                paths,
-            }),
-            Err(e) => StateChangesPage::Output(StateChangesPageOutput {
-                error_message: Some(e.to_string()),
-                paths: vec![],
-            }),
-        }
+        return StateChangesPage::Output(
+            get_state_changes_output(app_state, state_rt, named_values0, named_values1, n_steps)
+                .await,
+        );
     }
 }
 
-fn get_state_changes_input(
-    state_rt: Arc<RecordType>,
-    named_values0: HashMap<String, GoalTerm>,
-    named_values1: HashMap<String, GoalTerm>,
+async fn get_state_changes_input(
+    app_state: AppState,
+    state_rt: String,
+    named_values0: HashMap<String, String>,
+    named_values1: HashMap<String, String>,
     num_steps: i32,
 ) -> StateChangesPageInput {
+    let state_rt = app_state.facts.get_record_type(&state_rt).await.unwrap();
     let state_fields = state_rt.data_fields.clone();
 
     let start_state_values = state_fields
@@ -162,7 +139,7 @@ fn get_state_changes_input(
         .map(|field_name| {
             let value = named_values0
                 .get(field_name)
-                .map(|v| v.to_string().trim_matches('"').to_string())
+                .map(|v| v.trim_matches('"').to_string())
                 .unwrap_or("".to_string());
             (field_name.clone(), value)
         })
@@ -172,7 +149,7 @@ fn get_state_changes_input(
         .map(|field_name| {
             let value = named_values1
                 .get(field_name)
-                .map(|v| v.to_string().trim_matches('"').to_string())
+                .map(|v| v.trim_matches('"').to_string())
                 .unwrap_or("".to_string());
             (field_name.clone(), value)
         })
@@ -183,6 +160,56 @@ fn get_state_changes_input(
         start_state_values,
         end_state_values,
         num_steps,
+    }
+}
+
+async fn get_state_changes_output(
+    app_state: AppState,
+    state_rt: String,
+    named_values0: HashMap<String, String>,
+    named_values1: HashMap<String, String>,
+    num_steps: Option<&String>,
+) -> StateChangesPageOutput {
+    let parse_n_steps = || -> Result<i32, Box<dyn Error>> {
+        let n_steps = num_steps
+            .ok_or("Query parameter 'num-steps' not found")?
+            .parse::<i32>()
+            .map_err(|e| format!("Invalid num-steps argument: {}", e))?;
+        Ok(n_steps)
+    };
+
+    let n_steps = match parse_n_steps() {
+        Ok(i) => i,
+        Err(e) => {
+            return StateChangesPageOutput {
+                error_message: Some(format!("Failed to parse 'num-steps': {}", e)),
+                paths: vec![],
+            };
+        }
+    };
+
+    let named_values0 = named_values0
+        .into_iter()
+        .map(|(k, v)| (k.clone(), GoalTerm::String(v)))
+        .collect::<HashMap<_, _>>();
+    let named_values1 = named_values1
+        .into_iter()
+        .map(|(k, v)| (k.clone(), GoalTerm::String(v)))
+        .collect::<HashMap<_, _>>();
+
+    match app_state
+        .state_changes
+        .get_paths(&state_rt, named_values0, named_values1, n_steps)
+        .await
+    {
+        Ok(paths) => StateChangesPageOutput {
+            error_message: None,
+            paths,
+        },
+        Err(e) => StateChangesPageOutput {
+            error_message: Some(format!("Failed to get paths: {}", e)),
+            paths: vec![],
+        },
     }
 }
 
