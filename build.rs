1969d9eded8621ac688dd7b4c6e84a3082df82b3
Jonathan Jauhari <40555491+jonjau@users.noreply.github.com>
2025-04-27 14:29:25 +1000

    Dynamically write to internal pythia.pl file module imports and record glue predicates, via build script

--- build.rs
+++ build.rs
@@ -1,47 +1,124 @@
-// use std::{env, fs, path::Path};
+use std::{env, fs, io, path::Path, process::id};
 
 fn main() {
-//     let json_data =
-//         fs::read_to_string(Path::new("data/types.json")).expect("Failed to read JSON file");
-//     let objects: Vec<serde_json::Value> =
-//         serde_json::from_str(&json_data).expect("Invalid JSON format");
+    let json_data =
+        fs::read_to_string(Path::new("data/types.json")).expect("Failed to read JSON file");
+    let objects: Vec<serde_json::Value> =
+        serde_json::from_str(&json_data).expect("Invalid JSON format");
 
-//     let mut output = String::new();
+    let imports = objects
+        .iter()
+        .map(|o| {
+            o.get("name")
+                .and_then(|v| v.as_str())
+                .and_then(|name| Some(format!(":- use_module('./data/{}.pl').", name)))
+        })
+        .flatten()
+        .collect::<Vec<_>>()
+        .join("\n");
 
-//     let file_path = Path::new("data").join("changes.pl");
-//     let contents = fs::read_to_string(&file_path)
-//         .unwrap_or_else(|_| panic!("Could not find file: {:?}", file_path));
-//     output.push_str("% changes.pl\n"); // optional separator
-//     output.push_str(&contents);
-//     output.push_str("\n\n"); // optional separator
+    let record_predicates = objects
+        .iter()
+        .map(|o| {
+            let name = o.get("name").and_then(|v| v.as_str());
 
-//     let file_path = Path::new("data").join("record.pl");
-//     let contents = fs::read_to_string(&file_path)
-//         .unwrap_or_else(|_| panic!("Could not find file: {:?}", file_path));
-//     output.push_str("% record.pl\n"); // optional separator
-//     output.push_str(&contents);
-//     output.push_str("\n\n"); // optional separator
+            let id_fields = o
+                .get("id_fields")
+                .and_then(|vs| vs.as_array())
+                .and_then(|vs| Some(vs.iter().map(|v| v.as_str()).flatten().collect::<Vec<_>>()));
 
-//     for o in objects {
-//         if let Some(name) = o.get("name").and_then(|v| v.as_str()) {
-//             let file_path = Path::new("data").join(format!("{}.pl", name));
-//             let contents = fs::read_to_string(&file_path)
-//                 .unwrap_or_else(|_| panic!("Could not find file: {:?}", file_path));
-//             output.push_str(format!("% {}.pl\n", name).as_str()); // optional separator
-//             output.push_str(&contents);
-//             output.push_str("\n\n"); // optional separator
-//         }
-//     }
+            let data_fields = o
+                .get("data_fields")
+                .and_then(|vs| vs.as_array())
+                .and_then(|vs| Some(vs.iter().map(|v| v.as_str()).flatten().collect::<Vec<_>>()));
 
-//     // Write to output file inside OUT_DIR
-//     let out_dir = env::var("OUT_DIR").unwrap();
-//     let out_path = Path::new(&out_dir).join("pythia.pl");
-//     println!("cargo:warning=output path: {:?}", out_path);
-//     fs::write(&out_path, output).expect("Failed to write output file");
+            let metadata_fields = o
+                .get("metadata_fields")
+                .and_then(|vs| vs.as_array())
+                .and_then(|vs| Some(vs.iter().map(|v| v.as_str()).flatten().collect::<Vec<_>>()));
 
-//     //  fs::write("src/generated.rs", output).expect("Failed to write generated.rs");
-//     // Tell Cargo to rerun this build script if these files change
-//     println!("cargo:rerun-if-changed=data/types.json");
-//     println!("cargo:rerun-if-changed=data/");
+            if let (Some(name), Some(id_fields), Some(data_fields), Some(metadata_fields)) =
+                (name, id_fields, data_fields, metadata_fields)
+            {
+                if id_fields.is_empty() || metadata_fields.is_empty() {
+                    None
+                } else {
+                    Some(format!(
+                        "record({}, {}, [{}]) :-\n    {}({}, {}, {}).",
+                        metadata_fields.join(", "),
+                        id_fields.join(", "),
+                        data_fields.join(", "),
+                        name,
+                        id_fields.join(", "),
+                        data_fields.join(", "),
+                        metadata_fields.join(", ")
+                    ))
+                }
+            } else {
+                None
+            }
+        })
+        .flatten()
+        .collect::<Vec<_>>()
+        .join("\n\n");
+
+    println!("cargo:warning={:?}", record_predicates);
+
+    let start_marker = "% BEGIN fact-imports";
+    let end_marker = "% END fact-imports";
+    let _ = replace_section(
+        "data/internal/pythia.pl",
+        &imports,
+        start_marker,
+        end_marker,
+    );
+
+    let start_marker = "% BEGIN record-predicates";
+    let end_marker = "% END record-predicates";
+    let _ = replace_section(
+        "data/internal/pythia.pl",
+        &record_predicates,
+        start_marker,
+        end_marker,
+    );
+
+    // Tell Cargo to rerun this build script if these files change
+    println!("cargo:rerun-if-changed=data/types.json");
+    println!("cargo:rerun-if-changed=data/");
 }
 
+fn replace_section(
+    filepath: &str,
+    new_content: &str,
+    start_marker: &str,
+    end_marker: &str,
+) -> io::Result<()> {
+    let contents = fs::read_to_string(filepath)?;
+
+    // Find the start and end positions
+    if let (Some(start_pos), Some(end_pos)) =
+        (contents.find(start_marker), contents.find(end_marker))
+    {
+        let start_pos = start_pos + start_marker.len(); // Move past the start marker
+        let before = &contents[..start_pos];
+        let after = &contents[end_pos..];
+
+        // Build the new file contents
+        let mut new_file_contents = String::new();
+        new_file_contents.push_str(before);
+        new_file_contents.push_str("\n\n");
+        new_file_contents.push_str(new_content);
+        new_file_contents.push_str("\n\n");
+        new_file_contents.push_str(after);
+
+        fs::write(filepath, new_file_contents)?;
+    } else {
+        // Either marker is missing
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "Markers not found",
+        ));
+    }
+
+    Ok(())
+}
--- data/internal/pythia.pl
+++ data/internal/pythia.pl
@@ -1,13 +1,18 @@
-:- use_module(library(clpz)).
-:- use_module(library(lists)).
+% BEGIN fact-imports
 
 :- use_module('./data/dimlink.pl').
+:- use_module('./data/transaction.pl').
 :- use_module('./data/edge.pl').
 :- use_module('./data/arc.pl').
 
+% END fact-imports
+
+:- use_module(library(clpz)).
+:- use_module(library(lists)).
+
 change_step(Ctx, Id, Vals1, Vals2) :-
-    record(Ctx, _, SeqNum1, _, Id, Vals1),
-    record(Ctx, _, SeqNum2, _, Id, Vals2),
+    record(Ctx, _, _, SeqNum1, Id, Vals1),
+    record(Ctx, _, _, SeqNum2, Id, Vals2),
     number_chars(Num1, SeqNum1),
     number_chars(Num2, SeqNum2),
     Num2 #= Num1 + 1,
@@ -25,5 +30,13 @@ change_path(Ctx, Id, Vals1, Vals2, [Step|Steps]) :-
 % TODO: use a templating language to create these glue predicates:
 % if we try to get scryer-prolog to return an object like values(DRef, IRef, BegPeriod, EndPeriod)
 % it will not work, so I've opted to use the built-in prolog list.
-record(Context, EditTime, SeqNum, RecStatus, Id, [DRef, IRef, BegPeriod, EndPeriod]) :-
+
+% BEGIN record-predicates
+
+record(Context, EditTime, RecStatus, SeqNum, Id, [DRef, IRef, BegPeriod, EndPeriod]) :-
     dimlink(Id, DRef, IRef, BegPeriod, EndPeriod, Context, EditTime, RecStatus, SeqNum).
+
+record(Context, EditTime, RecStatus, SeqNum, Id, [DRef, IRef, BegPeriod, EndPeriod]) :-
+    transaction(Id, DRef, IRef, BegPeriod, EndPeriod, Context, EditTime, RecStatus, SeqNum).
+
+% END record-predicates
