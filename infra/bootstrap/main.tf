f1873a9bf9395af418aef9247f41c91a882599a0
Jonathan Jauhari <40555491+jonjau@users.noreply.github.com>
2025-07-24 12:54:44 +0000

    Add IAMFullAccess policy to GH Terraform bootstrap IAM Role

---
+++ infra/bootstrap/main.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role_policy_attachment" "role_policy_ecr" {
+  role       = "GithubActionsTerraformRole"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_s3" {
+  role       = "GithubActionsTerraformRole"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_iam" {
+  role       = "GithubActionsTerraformRole"
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_ecs" {
+  role       = "GithubActionsTerraformRole"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
