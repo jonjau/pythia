36969b03d6b5c4843d60ce23240b073febacf9f6
Jonathan Jauhari <40555491+jonjau@users.noreply.github.com>
2025-07-24 13:20:00 +0000

    Attach ECS task execution role to ECS task definition

--- infra/main.tf
+++ infra/main.tf
@@ -119,6 +119,7 @@ resource "aws_ecs_task_definition" "service" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
   memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
   container_definitions = jsonencode([
     {
       name  = "pythia-app"
