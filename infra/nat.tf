bef28728314e27cc4b974fd2fba91186d49b863f
Jonathan Jauhari <40555491+jonjau@users.noreply.github.com>
2025-07-27 11:26:24 +0000

    Move ECS instances into private subnets, add ALB in front in public subnet for security

--- infra/nat.tf
+++ infra/nat.tf
@@ -1,13 +1,12 @@
 module "fck_nat" {
   source  = "RaJiska/fck-nat/aws"
-  version = "1.3.0" # or latest stable version
+  version = "1.3.0"
 
   name      = "nat0"
   vpc_id    = aws_vpc.vpc.id
   subnet_id = aws_subnet.public[0].id
 
   update_route_tables = true
-
   route_tables_ids = {
     for i in range(var.az_count) :
     "private-${i}" => aws_route_table.private[i].id
